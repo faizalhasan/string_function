f85e86eddfdf9d9d3052aa55de8489746349c019
faizalhasan <113163991+faizalhasan@users.noreply.github.com>
2023-02-15 12:15:22 +0530

    created my_lib.h

---
+++ my_lib.h
@@ -0,0 +1,8 @@
+int length(char str[]);
+void str_cat(char input1[],char input2[]);
+void strcpy1(char str[],char str2[]);
+void strcpy2(char str[],char str2[],int n);
+char* str_chr(char *str,char c);
+void reverse(char arr[]);
+void mem_cpy(char *str, char *str2,int n);
+void demo_mmove(char *dest,char* src, int n);
