ee60249c31e324e3e896d46973e27013c96702a2
faizalhasan <113163991+faizalhasan@users.noreply.github.com>
2023-02-27 09:39:54 +0530

    updated my_lib.h

--- my_lib.h
+++ my_lib.h
@@ -4,6 +4,6 @@ void strcpy1(char str[],char str2[]);
 void strcpy2(char str[],char str2[],int n);
 char* str_chr(char *str,char c);
 void reverse(char arr[]);
-void mem_cpy(char *str, char *str2,int n);
+void* my_memcpy(void* dest, const void* src, size_t n);
 void demo_mmove(char *dest,char* src, int n);
 char* stringToken(char* s, char d);
